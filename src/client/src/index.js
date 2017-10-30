c19de57b7b1d877c986947ce5c16a125326fe160
Mark renzo Santiago <ms103804@gmail.com>
2017-10-30 12:45:31 -0700

    update client side react rendering

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,34 +1,8 @@
-/**
- * Client entry point
- */
+/*
+    ./client/index.js
+*/
 import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import App from './App';
-import { configureStore } from './store';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-// Initialize store
-const store = configureStore(window.__INITIAL_STATE__);
-const mountApp = document.getElementById('root');
-
-render(
-    <AppContainer>
-        <App store={store} />
-    </AppContainer>,
-    mountApp
-);
-
-// For hot reloading of react components
-if (module.hot) {
-    module.hot.accept('./App', () => {
-        // If you use Webpack 2 in ES modules mode, you can
-        // use <App /> here rather than require() a <NextApp />.
-        const NextApp = require('./App').default; // eslint-disable-line global-require
-        render(
-            <AppContainer>
-                <NextApp store={store} />
-            </AppContainer>,
-            mountApp
-        );
-    });
-}
+ReactDOM.render(<App />, document.getElementById('root'));
