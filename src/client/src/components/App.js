6d8221c3b7dd72f500ec4fb5d8e206692f5eefd0
Mark renzo Santiago <ms103804@gmail.com>
2017-11-27 11:33:37 -0800

    clean up main page

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -28,6 +28,24 @@ export default class App extends Component {
         return (
             <div>
                 <div className="ui vertical masthead center aligned segment">
+
+                    <div className="ui container">
+                        <div className="ui large secondary pointing menu">
+                            <a className="toc item">
+                                <i className="sidebar icon"></i>
+                            </a>
+                            <div className="left item">
+                                <i className="food icon"></i>
+                            </div>
+                            <div className="right item">
+                                <a href="board.html" className="item">Bounty</a>
+                                <a className="item">Explore</a>
+                                <a className="item">Blog</a>
+                                <a href="login.html" className="ui button">Log in</a>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className = "Header">
                         <h1>Send-Fudes now 👊</h1>
                     </div>
