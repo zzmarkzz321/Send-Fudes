567947b2003c4f9671acdac7b8e36ecf81e23396
Mark renzo Santiago <ms103804@gmail.com>
2017-11-27 11:26:51 -0800

    add css loader for styling

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 
+
 export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            search_value: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            search_value: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.search_value);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
@@ -8,14 +31,19 @@ export default class App extends Component {
                     <div className = "Header">
                         <h1>Send-Fudes now 👊</h1>
                     </div>
-                </div>
 
-                <div className = "Verification">
-                    <div className="ui category search">
-                        <div className="ui icon input">
-                            <h1>Hello</h1>
+                    <div className = "Verification">
+                        <div className="ui category search">
+                            <div className="ui icon input">
+                                <form onSubmit={this.handleSubmit}>
+                                    <label>
+                                        <input type="text" value={this.state.search_value} onChange={this.handleChange} />
+                                    </label>
+                                    <input type="submit" value="Submit" />
+                                </form>
+                            </div>
+                            <div className="results"></div>
                         </div>
-                        <div className="results"></div>
                     </div>
                 </div>
             </div>
