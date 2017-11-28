5f713379ff875fc4b36497951178660e5b3d382a
Mark renzo Santiago <ms103804@gmail.com>
2017-11-27 22:02:49 -0800

    Broman stuff

--- src/server/src/services/board/index.js
+++ src/server/src/services/board/index.js
@@ -1,1 +1,41 @@
 'use strict';
+
+/*
+* 1. CRUD
+* 2. JSON return schema
+* */
+
+function Board() {
+}
+
+Board.prototype.getSingleJob = (job_id) => {
+    return new Promise((resolve, reject) => {
+        // TODO: DO OUT DATABASE CRAP HERE
+
+        // EXAMPLE RESULT TODO: PLEASE MODIFY
+        const resultJobDescription = {
+            restaurant_name: 'in-n-out',
+            rating: 4,
+            location: 'blah blah 16th street, Merced',
+            request: [
+                'double double',
+                'fries',
+                'drink'
+            ],
+            requester: 1013984734,
+            accepted: false,
+            date: '12-07-2017-1900'
+        };
+
+        // TODO If your database crap works
+        resolve(resultJobDescription);
+
+        // TODO ELSE, you'll reject with an error
+        reject({'message': 'This shit doesnt work'});
+    });
+};
+
+module.exports = Board;
+
+
+
