25781b4efd9a7ba0797573ed00af134e907e2bbc
wyattchen <wyatt6388@gmail.com>
2017-12-11 20:53:37 -0800

    update payment.js
    indent and added <br/>, check if it is at the right place

--- src/client/src/components/pages/payment.js
+++ src/client/src/components/pages/payment.js
@@ -6,47 +6,48 @@ export class Payment extends Component {
         return (
             <div>
                 <h1 className ="payment">Ready to Pay?
-                <i  className ="fa fa-credit-card-alt"> </i>
+                    <i  className ="fa fa-credit-card-alt"> </i>
                 </h1>
 
-                <table>
-                    <tr>
-                    <th>Items</th>
+                    <table>
+                        <tr>
+                        <th>Items</th>
 
-                    <th>Price</th>
-                    </tr>
-                    <tr>
-                    <td>Double Double</td>
-                    <td>$ 3.45</td>
+                        <th>Price</th>
+                        </tr>
+                        <tr>
+                        <td>Double Double</td>
+                        <td>$ 3.45</td>
 
-                    </tr>
-                    <tr>
-                    <td>Fries</td>
-                    <td>$ 1.60</td>
+                        </tr>
+                        <tr>
+                        <td>Fries</td>
+                        <td>$ 1.60</td>
 
-                    </tr>
-                    <tr>
-                    <td>Medium Drink</td>
-                    <td>$ 1.65</td>
+                        </tr>
+                        <tr>
+                        <td>Medium Drink</td>
+                        <td>$ 1.65</td>
 
-                    </tr>
-                    <tr>
-                    <td>Tax + Fee</td>
+                        </tr>
+                        <tr>
+                        <td>Tax + Fee</td>
 
-                    <td>$ 3.75</td>
+                        <td>$ 3.75</td>
 
-                    </tr>
-                    <tr>
-                    <td>Total</td>
-                    <td>$ 10.45</td>
-                    </tr>
+                        </tr>
+                        <tr>
+                        <td>Total</td>
+                        <td>$ 10.45</td>
+                        </tr>
 
                     </table>
                     <br>
-                <button className ="ui button" type="submit">Pay Now!
-                </button>
-                <i className ="fa fa-cc-paypal"style="font-size:30px;color:dodgerblue;"></i>
-                <i className ="fa fa-cc-visa"style="font-size:30px;color:red;"></i>
+                        <button className ="ui button" type="submit">Pay Now!
+                        </button>
+                        <i className ="fa fa-cc-paypal"style="font-size:30px;color:dodgerblue;"></i>
+                        <i className ="fa fa-cc-visa"style="font-size:30px;color:red;"></i>
+                    <br/>
             </div>
             );
     }
