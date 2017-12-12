1582142cc17151c98a369c10ba0c4309fa313420
rowong20 <rowong20@gmail.com>
2017-12-11 17:42:42 -0800

    update index.js
    added backend code for board, implementing code for querying the database.

--- src/server/src/services/board/index.js
+++ src/server/src/services/board/index.js
@@ -1,4 +1,6 @@
 'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 /*
 * 1. CRUD
@@ -9,11 +11,30 @@ function Board() {
 }
 
 Board.prototype.getSingleJob = (job_id) => {
+    //Gets a single job defined by job_id
     return new Promise((resolve, reject) => {
-        // TODO: DO OUT DATABASE CRAP HERE
+        // Currently not testing proximity or ratings for now
+        const jobSchema = new Schema({
+            restaurant_name: { type: 'String', required: true },
+            rating: { type: 'Number', required: false },
+            proximity: { type: 'Number', required: false },
+            location: { type: 'String', required: true },
+            request: { type: 'String', required: true },
+            dateAdded: { type: 'Date', default: Date.now, required: true },
+            requester: { type: 'String', required: true },
+            accepted: { type: 'Boolean', required: true },
+            job_id: {type: 'Number', required: true},
+        });
 
-        // EXAMPLE RESULT TODO: PLEASE MODIFY
-        const resultJobDescription = {
+        var Job = mongoose.model('Job', jobSchema);
+        Job.find({ job_id: job_id}, function (results) {
+            resolve(results);
+        });
+        reject({'message': 'This shit doesnt work'});
+
+        /*
+        // EXAMPLE RESULT
+        const resultJobDescription = new Schema( {
             restaurant_name: 'in-n-out',
             rating: 4,
             proximity: 4.5,
@@ -25,16 +46,40 @@ Board.prototype.getSingleJob = (job_id) => {
             ],
             requester: 1013984734,
             accepted: false,
-            date: '12-07-2017-1900'
-        };
+            date: '12-07-2017-1900',
+            job_id: 12341512334
+        });
+        */
+    });
+};
+Board.prototype.getMultipleJobs = (job_amount) => {
 
-        // TODO If your database crap works
-        if (resultJobDescription)
-            resolve(resultJobDescription);
+     //   Gets job_amount of jobs to fill the board within the hour, sorted in descending order.
 
-        // TODO ELSE, you'll reject with an error
-        else
-            reject({'message': 'This shit doesnt work'});
+    return new Promise((resolve, reject) => {
+        const jobSchema = new Schema({
+            restaurant_name: { type: 'String', required: true },
+            rating: { type: 'Number', required: false },
+            proximity: { type: 'Number', required: false },
+            location: { type: 'String', required: true },
+            request: { type: 'String', required: true },
+            dateAdded: { type: 'Date', default: Date.now, required: true },
+            requester: { type: 'String', required: true },
+            accepted: { type: 'Boolean', required: true },
+            job_id: {type: 'Number', required: true},
+        });
+        var Job = mongoose.model('Job', jobSchema);
+        var time= new Date().getTime()-60*60*1000;
+        //Example code for my reference for sorting
+        //Room.find({}).sort({date: 'descending'}).exec(function(err, docs)
+        let resultJob=Job.
+            find({ accepted: false }).
+            where('dateAdded').greaterThanOrEqual(time).
+            limit(job_amount).
+            sort({dateAdded: 'descending'}).
+            exec();
+        resolve(resultJob);
+        reject({'message': 'This shit doesnt work'});
     });
 };
 module.exports = Board;
